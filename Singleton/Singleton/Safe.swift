0b049d504e7c7697b8e3a6520351c421569a49fb
Dmitry Shapkin <shapkindev@gmail.com>
2019-01-10 15:10:20 +0300

    Sixth commit

---
+++ Singleton/Singleton/Safe.swift
@@ -0,0 +1,15 @@
+//
+//  Safe.swift
+//  Singleton
+//
+//  Created by Администратор on 09/01/2019.
+//  Copyright © 2019 Администратор. All rights reserved.
+//
+
+class Safe {
+    
+    var money = 0
+    static let shared = Safe()
+    
+}
+
